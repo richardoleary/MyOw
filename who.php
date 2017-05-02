918c3b16bd56603558cc9ec7f8452e15f57c27e6
REWE50 <raymond.egan@webelevate.ie>
2017-05-02 01:07:36 +0100

    Add files via upload

--- who.php
+++ who.php
@@ -86,15 +86,14 @@
             </div><!-- end spacer row -->
             
         	<!-- start main row 1 -->
-            <div class="row">
+            <div class="row container">
             	<div class="col-lg-10 col-lg-offsfet-1 col-md-10 col-md-offset-1">
                 	<div class="row">
                     	<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
                         	<img src="images/persona.png" class="img-responsive">
                         </div>
                     </div>
-                   	
-	            </div>
+	           </div>
             </div><!-- end main row 1 -->
             
             <!--spacer -->
